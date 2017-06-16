8e99f61e029c5c8c004d34bb5f54cec003153db0
Baldeep <baldeepsingh.93@gmail.com>
2017-06-16 20:09:00 +0100

    Added fastLED Arduino library, found brightness and speed values

--- Arduino/led_test/led_test.ino
+++ Arduino/led_test/led_test.ino
@@ -1,7 +1,7 @@
 #include <FastLED.h>
 
 #define LED_PIN     5
-#define NUM_LEDS    300 // 60 per meter
+#define NUM_LEDS    300 // 60 per meter (300 max)
 #define BRIGHTNESS  64
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
@@ -15,12 +15,17 @@ CRGBPalette16 currentPalette;
 // The way the LEDs fade
 TBlendType    currentBlending;
 
+uint8_t brightness = 255; // (0 - 255)
+
+uint8_t pause = 5000; // (min 100)
+
 void setup() {
     delay( 3000 ); // power-up safety delay
     FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
     FastLED.setBrightness(  BRIGHTNESS );
     
-    currentPalette = RainbowColors_p;
+    currentPalette = RainbowStripeColors_p;
+    fill_solid( currentPalette, 16, CRGB::Green);
     currentBlending = LINEARBLEND;
 }
 
@@ -29,18 +34,16 @@ void loop() {
   static uint8_t startIndex = 0;
   startIndex = startIndex + 1; /* motion speed */
 
-  uint8_t brightness = 255;
-  
+
   FillLEDsFromPaletteColors( startIndex);
 
   FastLED.show();
-  FastLED.delay(1000 / UPDATES_PER_SECOND);
+  FastLED.delay(pause / UPDATES_PER_SECOND);
 }
 
 void FillLEDsFromPaletteColors( uint8_t colorIndex)
 {
-    uint8_t brightness = 255;
-    
+ 
     for( int i = 0; i < NUM_LEDS; i++) {
         leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
         colorIndex += 3;
