768a893cbead40388c418574a527d7aac985bc46
Baldeep <baldeepsingh.93@gmail.com>
2017-06-18 01:00:23 +0100

    LED strip brightness now increases when pressing a button connected to pin 2, to a max of 250, and then drops to 0 again.

--- Arduino/led_test/led_test.ino
+++ Arduino/led_test/led_test.ino
@@ -16,21 +16,54 @@ CRGBPalette16 currentPalette;
 TBlendType    currentBlending;
 
 uint8_t brightness = 255; // (0 - 255)
-
 uint8_t pause = 5000; // (min 100)
 
+
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+
+int buttonState = 0;         // variable for reading the pushbutton status
+
+int buttonPressed = 0;
+
 void setup() {
+
+    // initialize the LED pin as an output:
+    pinMode(ledPin, OUTPUT);
+    // initialize the pushbutton pin as an input:
+    pinMode(buttonPin, INPUT);
+  
     delay( 3000 ); // power-up safety delay
     FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
     FastLED.setBrightness(  BRIGHTNESS );
     
-    currentPalette = RainbowStripeColors_p;
-    fill_solid( currentPalette, 16, CRGB::Green);
+    currentPalette = RainbowColors_p;
     currentBlending = LINEARBLEND;
 }
 
 void loop() {
 
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    if(buttonPressed == 0){
+      buttonPressed = 1;
+      brightness = brightness + 25;
+      if(brightness > 255){
+        brightness = 0;
+      }
+      digitalWrite(ledPin, HIGH);
+    }
+  } else {
+    // turn LED off:
+    buttonPressed = 0;
+    digitalWrite(ledPin, LOW);
+  }
+  
   static uint8_t startIndex = 0;
   startIndex = startIndex + 1; /* motion speed */
 
